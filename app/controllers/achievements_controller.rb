551143e1c77dc9925b235ed7ad00d5fcf8b945fb
ramin rostami <rm.rostami@gmail.com>
2018-01-18 19:27:58 +0000

    sad Path : when user dont fill fields
    it must be come back some error user when fields are empty,cant be blank

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -7,6 +7,8 @@ class AchievementsController < ActionController::Base
     @achievement = Achievement.new(achievement_params)
     if @achievement.save
       redirect_to root_url, notice: 'Achievement has been created'
+    else
+      render :new
     end
   end
   
