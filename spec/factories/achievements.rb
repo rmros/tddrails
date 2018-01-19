9c0d0fde39cfec01d48000ba08b95d69f5b41801
ramin rostami <rm.rostami@gmail.com>
2018-01-19 07:36:35 +0000

    added factory_girl_rails gem

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :achievement do
-    title "MyString"
-    description "MyText"
-    privacy 1
+    title "title"
+    description "description"
+    privacy Achievement.privacies[:private_access]
     featured false
-    cover_image "MyString"
+    cover_image "some_file.png"
   end
 end
--- spec/features/achievement_page_spec.rb
+++ spec/features/achievement_page_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 feature 'achievement page' do
     scenario 'achievement public page' do
-      achievement = Achievement.create(title: 'Just did it')
+      achievement = FactoryGirl.create(:achievement)
       visit("/achievements/#{achievement.id}")
       
       expect(page).to have_content('Just did it')
     end
     
     scenario 'render markdown description' do
-      achievement = Achievement.create(description: 'That *was* hard')
+      achievement = Achievement.create!(title: 'Just did it', description: 'That *was* hard')
       visit("/achievements/#{achievement.id}")
       
       expect(page).to have_content('<i>was</i>')
