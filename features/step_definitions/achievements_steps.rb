02e79d2a01344d15199266623796d1eafcc09aa5
ramin rostami <rm.rostami@gmail.com>
2018-01-20 07:45:02 +0000

    changed achievements cucumber

--- features/step_definitions/achievements_steps.rb
+++ features/step_definitions/achievements_steps.rb
@@ -1,14 +1,14 @@
-Given("I am a guest user") do
-end
+# Given("I am a guest user") do
+# end
 
-Given("there is a public achievement") do
-  @achievement=FactoryGirl.create(:public_achievement, title: 'i did it')
-end
+# Given("there is a public achievement") do
+#   @achievement=FactoryGirl.create(:public_achievement, title: 'i did it')
+# end
 
-When("I go to see achievement's page") do
-    visit(achievement_path(@achievement.id))
-end
+# When("I go to see achievement's page") do
+#     visit(achievement_path(@achievement.id))
+# end
 
-Then("I must see achievement's content") do
-  expect(page).to have_content('i did it')
-end
+# Then("I must see achievement's content") do
+#   expect(page).to have_content('i did it')
+# end
